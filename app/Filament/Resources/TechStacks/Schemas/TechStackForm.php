74b1690630e604dc760f1aea4763ae3c6e0e7fff
Konrad <k.adamczyk81@gmail.com>
2025-09-09 12:41:35 +0200

    Resources files for Filament admin panel

---
+++ app/Filament/Resources/TechStacks/Schemas/TechStackForm.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Resources\TechStacks\Schemas;
+
+use Filament\Schemas\Schema;
+
+class TechStackForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                //
+            ]);
+    }
+}
